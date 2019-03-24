475eb254725fe8566bb3294b8254aa49020475b4
sysadmin <sysadmin@devops.com>
2019-03-24 15:27:10 +0800

    创建项目

---
+++ src/main/java/com/first/ema/EmaApplication.java
@@ -0,0 +1,13 @@
+package com.first.ema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/first/ema/EmaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.first.ema;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
